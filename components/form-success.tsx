d963eb6df3abd97ebda7a70fdf03787f225a6a4d
printwhoishere <uvarov_2011@list.ru>
2024-03-04 02:07:37 +0300

    add middleware and login:   actions/auth.ts 	add middleware and login:   app/(protected)/settings/page.tsx 	add middleware and login:   app/api/auth/[...nextauth]/route.ts 	add middleware and login:   auth.config.ts 	add middleware and login:   auth.ts 	add color for dark theme:   components/form-error.tsx 	add color for dark theme:   components/form-success.tsx 	add middleware and login:   middleware.ts 	modified:   package.json 	modified:   pnpm-lock.yaml 	add middleware and login:   routes.ts

--- components/form-success.tsx
+++ components/form-success.tsx
@@ -8,7 +8,7 @@ export const FormSuccess = ({ message }: FormSuccessProps) => {
 	if (!message) return null
 
 	return (
-		<div className='bg-emerald-500/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-emerald-500'>
+		<div className='bg-emerald-500/15 dark:bg-emerald-500/70 p-3 rounded-md flex items-center gap-x-2 text-sm text-emerald-500 dark:text-black'>
 			<CheckCircle />
 			<p>{message}</p>
 		</div>
